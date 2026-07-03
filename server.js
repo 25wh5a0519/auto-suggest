f2423cf86ecee6f3bab163565ad07f1b70ff39c1
swetha <25wh5a0519@bvrithyderabad.edu.in>
2026-07-03 13:41:34 +0530

    update port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = 3002;
+//changed this 
+const PORT = process.env.PORT || 3002;
 // web server : 
 app.use(express.static("frontend"))
 app.listen(PORT, function()
